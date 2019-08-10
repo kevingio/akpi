904e3fb3b16de6053792f01bb744bfc3ff5ae1c4
Kevin Giovanni <kevin.givoanni@tokopedia.com>
2019-08-10 08:17:15 +0700

    init project

---
+++ database/migrations/2019_08_09_113922_create_members_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('member_no');
+            $table->string('name');
+            $table->string('location');
+            $table->string('phone_number');
+            $table->enum('type', ['pastoral counselor', 'anggota kehormatan']);
+            $table->string('status');
+            $table->string('avatar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
